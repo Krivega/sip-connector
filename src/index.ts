9ea20f5e95bf32a04ff5cb0dd25912bdeb9e8b56
Krivega Dmitriy <mr.krivega@gmail.com>
2021-04-23 13:17:04 +0300

    fix: add export resolveVideoSendingBalancer to index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import SipConnector, { hasCanceledCallError } from './SipConnector';
+import resolveVideoSendingBalancer from './videoSendingBalancer‎';
 import * as causes from './causes';
 import * as eventNames from './eventNames';
 
-export { causes, eventNames, hasCanceledCallError };
+export { causes, eventNames, hasCanceledCallError, resolveVideoSendingBalancer };
 
 export default SipConnector;
