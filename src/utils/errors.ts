aacbf246db30b577d11146bf145b1d4a31312642
a.gulevich@vinteo.ru <a.gulevich@vinteo.ru>
2024-06-17 19:21:20 +0300

    fix: compare websocket response with error instead array of errors

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,20 +1,11 @@
 const DECLINE_ERROR_MESSAGE = 'Error decline with 603';
 const HANDSHAKE_WEBSOCKET_OPENING_ERROR_CODE = 1006;
 
-const hasHandshakeWebsocketOpeningError = (error: unknown): boolean => {
+export const hasHandshakeWebsocketOpeningError = (error: unknown): boolean => {
   const isObject = typeof error === 'object' && error !== null;
   const isErrorWithCodeProperty = isObject && 'code' in error;
 
   return isErrorWithCodeProperty && error.code === HANDSHAKE_WEBSOCKET_OPENING_ERROR_CODE;
-};
-
-export const hasIncludesHandshakeWebsocketOpeningError = (errors: unknown): boolean => {
-  return (
-    Array.isArray(errors) &&
-    errors.some((element) => {
-      return hasHandshakeWebsocketOpeningError(element);
-    })
-  );
 };
 
 export const hasDeclineResponseFromServer = (error: Error): boolean => {
