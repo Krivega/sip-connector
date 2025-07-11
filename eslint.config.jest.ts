2d168ab42d6eee47d3bdad6183d1cdddbf6e814a
Krivega <mr.krivega@gmail.com>
2025-07-11 18:10:08 +0300

    chore: update dependencies

---
+++ eslint.config.jest.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import type { Linter } from 'eslint';
+import pluginJest from 'eslint-plugin-jest';
+import config from './eslint.config.base';
+
+const jestConfig: Linter.Config[] = [
+  ...config,
+  {
+    files: ['**/*.spec.js', '**/*.test.js', '**/__tests__/**/*.ts', '**/__tests__/**/*.tsx'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      ...pluginJest.configs['flat/recommended'].rules,
+    },
+  },
+];
+
+export default jestConfig;
---
+++ eslint.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { includeIgnoreFile } from '@eslint/compat';
+import { defineConfig } from 'eslint/config';
+import nodeUrl from 'node:url';
+import configJest from './eslint.config.jest';
+
+const gitignorePath = nodeUrl.fileURLToPath(new URL('.gitignore', import.meta.url));
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  {
+    extends: [configJest],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: './',
+      },
+    },
+  },
+]);
