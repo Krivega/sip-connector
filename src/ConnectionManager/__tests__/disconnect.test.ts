7f847919a5b4bd528a17b58f8d0c01ad6cb7d0a5
Krivega <mr.krivega@gmail.com>
2025-07-19 00:03:50 +0300

    refactor: update ConfigurationManager methods and improve SIP connection handling

---
+++ src/ConnectionManager/__tests__/disconnect.test.ts
@@ -0,0 +1,27 @@
+/// <reference types="jest" />
+import { dataForConnectionWithAuthorization } from '../../__fixtures__';
+import jssip from '../../__fixtures__/jssip.mock';
+import type { TJsSIP } from '../../types';
+import ConnectionManager from '../ConnectionManager';
+
+describe('disconnect', () => {
+  let connectionManager: ConnectionManager;
+
+  beforeEach(() => {
+    connectionManager = new ConnectionManager({
+      JsSIP: jssip as unknown as TJsSIP,
+    });
+  });
+
+  it('должен отключать пользователя с авторизацией', async () => {
+    expect.assertions(2);
+
+    await connectionManager.connect(dataForConnectionWithAuthorization);
+
+    expect(connectionManager.isConfigured()).toBe(true);
+
+    await connectionManager.disconnect();
+
+    expect(connectionManager.isConfigured()).toBe(false);
+  });
+});
