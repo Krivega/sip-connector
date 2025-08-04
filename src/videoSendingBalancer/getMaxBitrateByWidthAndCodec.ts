b8b5488d330b2ab6b9b809fb55dfb3d740805423
Krivega <mr.krivega@gmail.com>
2025-08-04 14:48:16 +0300

    fix: set max bitrate

--- src/videoSendingBalancer/getMaxBitrateByWidthAndCodec.ts
+++ src/videoSendingBalancer/getMaxBitrateByWidthAndCodec.ts
@@ -1,8 +1,12 @@
-import getMaxBitrateByWidth, { MINIMUM_BITRATE } from './getMaxBitrateByWidth';
+import getMaxBitrateByWidth, { MAXIMUM_BITRATE, MINIMUM_BITRATE } from './getMaxBitrateByWidth';
 import scaleBitrateByCodec from './scaleBitrateByCodec';
 
 export const getMinimumBitrate = (codec?: string) => {
   return scaleBitrateByCodec(MINIMUM_BITRATE, codec);
+};
+
+export const getMaximumBitrate = (codec?: string) => {
+  return scaleBitrateByCodec(MAXIMUM_BITRATE, codec);
 };
 
 const getMaxBitrateByWidthAndCodec = (maxWidth: number, codec?: string): number => {
