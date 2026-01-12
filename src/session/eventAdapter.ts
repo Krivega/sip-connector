1c57dbf87b126f159fdc041f12231f6df20fdf9e
Krivega <mr.krivega@gmail.com>
2026-01-12 21:27:11 +0300

    refactor: simplify event subscription handling in session event adapter

--- src/session/eventAdapter.ts
+++ src/session/eventAdapter.ts
@@ -1,11 +1,8 @@
-import type { TEvents as TCallEvents, TEventMap as TCallEventMap } from '@/CallManager/eventNames';
-import type {
-  TEvents as TConnectionEvents,
-  TEventMap as TConnectionEventMap,
-} from '@/ConnectionManager/eventNames';
+// import type { TypedEvents } from 'events-constructor';
+import type { TEvents as TCallEvents } from '@/CallManager/eventNames';
+import type { TEvents as TConnectionEvents } from '@/ConnectionManager/eventNames';
 import type {
   TEvents as TIncomingEvents,
-  TEventMap as TIncomingEventMap,
   TRemoteCallerData,
 } from '@/IncomingCallManager/eventNames';
 import type { TSessionActor } from './rootMachine';
@@ -16,31 +13,11 @@ export type TSessionEventAdapterDeps = {
   incomingCallEvents: TIncomingEvents;
 };
 
-type Unsubscribe = () => void;
-
-type EventEmitter<TEventMap> = {
-  on: <K extends keyof TEventMap>(eventName: K, handler: (data: TEventMap[K]) => void) => void;
-  off: <K extends keyof TEventMap>(eventName: K, handler: (data: TEventMap[K]) => void) => void;
-};
-
-const subscribe = <TEventMap, K extends keyof TEventMap>(
-  emitter: EventEmitter<TEventMap>,
-  eventName: K,
-  handler: (data: TEventMap[K]) => void,
-  subscriptions: Unsubscribe[],
-  // eslint-disable-next-line @typescript-eslint/max-params
-) => {
-  emitter.on(eventName, handler);
-  subscriptions.push(() => {
-    emitter.off(eventName, handler);
-  });
-};
-
 export const attachSessionEventAdapter = (
   actor: TSessionActor,
   deps: TSessionEventAdapterDeps,
 ): (() => void) => {
-  const subscriptions: Unsubscribe[] = [];
+  const subscriptions: (() => void)[] = [];
   let hasIncomingCall = false;
 
   const consumeIncoming = () => {
@@ -56,229 +33,157 @@ export const attachSessionEventAdapter = (
   };
 
   // Connection events
-  const connectionEmitter = deps.connectionEvents as unknown as EventEmitter<TConnectionEventMap>;
-  const callEmitter = deps.callEvents as unknown as EventEmitter<TCallEventMap>;
-  const incomingEmitter = deps.incomingCallEvents as unknown as EventEmitter<TIncomingEventMap>;
+  const connectionEmitter = deps.connectionEvents;
+  const callEmitter = deps.callEvents;
+  const incomingEmitter = deps.incomingCallEvents;
 
-  subscribe(
-    connectionEmitter,
-    'connect-started',
-    () => {
+  subscriptions.push(
+    connectionEmitter.on('connect-started', () => {
       actor.send({ type: 'CONNECTION.START' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    connectionEmitter,
-    'connecting',
-    () => {
+  subscriptions.push(
+    connectionEmitter.on('connecting', () => {
       actor.send({ type: 'CONNECTION.START' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    connectionEmitter,
-    'connect-parameters-resolve-success',
-    () => {
+  subscriptions.push(
+    connectionEmitter.on('connect-parameters-resolve-success', () => {
       actor.send({ type: 'CONNECTION.INIT' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    connectionEmitter,
-    'connected',
-    () => {
+  subscriptions.push(
+    connectionEmitter.on('connected', () => {
       actor.send({ type: 'CONNECTION.CONNECTED' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    connectionEmitter,
-    'registered',
-    () => {
+  subscriptions.push(
+    connectionEmitter.on('registered', () => {
       actor.send({ type: 'CONNECTION.REGISTERED' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    connectionEmitter,
-    'unregistered',
-    () => {
+  subscriptions.push(
+    connectionEmitter.on('unregistered', () => {
       actor.send({ type: 'CONNECTION.UNREGISTERED' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    connectionEmitter,
-    'disconnected',
-    () => {
+  subscriptions.push(
+    connectionEmitter.on('disconnected', () => {
       actor.send({ type: 'CONNECTION.DISCONNECTED' });
       actor.send({ type: 'CALL.ENDED' });
       actor.send({ type: 'SCREEN.ENDED' });
       actor.send({ type: 'INCOMING.CLEAR' });
       hasIncomingCall = false;
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    connectionEmitter,
-    'registrationFailed',
-    (error) => {
+  subscriptions.push(
+    connectionEmitter.on('registrationFailed', (error) => {
       actor.send({ type: 'CONNECTION.FAILED', error });
       actor.send({ type: 'CALL.ENDED' });
       actor.send({ type: 'SCREEN.ENDED' });
       actor.send({ type: 'INCOMING.CLEAR' });
       hasIncomingCall = false;
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    connectionEmitter,
-    'connect-failed',
-    (error) => {
+  subscriptions.push(
+    connectionEmitter.on('connect-failed', (error) => {
       actor.send({ type: 'CONNECTION.FAILED', error });
       actor.send({ type: 'CALL.ENDED' });
       actor.send({ type: 'SCREEN.ENDED' });
       actor.send({ type: 'INCOMING.CLEAR' });
       hasIncomingCall = false;
-    },
-    subscriptions,
+    }),
   );
 
   // Call events
-  subscribe(
-    callEmitter,
-    'connecting',
-    () => {
+  subscriptions.push(
+    callEmitter.on('connecting', () => {
       actor.send({ type: 'CALL.CONNECTING' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    callEmitter,
-    'progress',
-    () => {
+  subscriptions.push(
+    callEmitter.on('progress', () => {
       actor.send({ type: 'CALL.RINGING' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    callEmitter,
-    'accepted',
-    () => {
+  subscriptions.push(
+    callEmitter.on('accepted', () => {
       actor.send({ type: 'CALL.ACCEPTED' });
       consumeIncoming();
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    callEmitter,
-    'confirmed',
-    () => {
+  subscriptions.push(
+    callEmitter.on('confirmed', () => {
       actor.send({ type: 'CALL.CONFIRMED' });
       consumeIncoming();
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    callEmitter,
-    'ended',
-    () => {
+  subscriptions.push(
+    callEmitter.on('ended', () => {
       actor.send({ type: 'CALL.ENDED' });
       clearIncoming();
       actor.send({ type: 'SCREEN.ENDED' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    callEmitter,
-    'failed',
-    (error) => {
+  subscriptions.push(
+    callEmitter.on('failed', (error) => {
       actor.send({ type: 'CALL.FAILED', error });
       clearIncoming();
       actor.send({ type: 'SCREEN.FAILED', error });
-    },
-    subscriptions,
+    }),
   );
 
   // Screen share (presentation) events
-  subscribe(
-    callEmitter,
-    'presentation:start',
-    () => {
+  subscriptions.push(
+    callEmitter.on('presentation:start', () => {
       actor.send({ type: 'SCREEN.STARTING' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    callEmitter,
-    'presentation:started',
-    () => {
+  subscriptions.push(
+    callEmitter.on('presentation:started', () => {
       actor.send({ type: 'SCREEN.STARTED' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    callEmitter,
-    'presentation:end',
-    () => {
+  subscriptions.push(
+    callEmitter.on('presentation:end', () => {
       actor.send({ type: 'SCREEN.ENDING' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    callEmitter,
-    'presentation:ended',
-    () => {
+  subscriptions.push(
+    callEmitter.on('presentation:ended', () => {
       actor.send({ type: 'SCREEN.ENDED' });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    callEmitter,
-    'presentation:failed',
-    (error) => {
+  subscriptions.push(
+    callEmitter.on('presentation:failed', (error) => {
       actor.send({ type: 'SCREEN.FAILED', error });
-    },
-    subscriptions,
+    }),
   );
 
   // Incoming call events
-  subscribe(
-    incomingEmitter,
-    'incomingCall',
-    (data: TRemoteCallerData) => {
+  subscriptions.push(
+    incomingEmitter.on('incomingCall', (data: TRemoteCallerData) => {
       hasIncomingCall = true;
       actor.send({ type: 'INCOMING.RINGING', data });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    incomingEmitter,
-    'declinedIncomingCall',
-    (data: TRemoteCallerData) => {
+  subscriptions.push(
+    incomingEmitter.on('declinedIncomingCall', (data: TRemoteCallerData) => {
       hasIncomingCall = false;
       actor.send({ type: 'INCOMING.DECLINED', data });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    incomingEmitter,
-    'terminatedIncomingCall',
-    (data: TRemoteCallerData) => {
+  subscriptions.push(
+    incomingEmitter.on('terminatedIncomingCall', (data: TRemoteCallerData) => {
       hasIncomingCall = false;
       actor.send({ type: 'INCOMING.TERMINATED', data });
-    },
-    subscriptions,
+    }),
   );
-  subscribe(
-    incomingEmitter,
-    'failedIncomingCall',
-    (data: TRemoteCallerData) => {
+  subscriptions.push(
+    incomingEmitter.on('failedIncomingCall', (data: TRemoteCallerData) => {
       hasIncomingCall = false;
       actor.send({ type: 'INCOMING.FAILED', data });
-    },
-    subscriptions,
+    }),
   );
 
   return () => {
