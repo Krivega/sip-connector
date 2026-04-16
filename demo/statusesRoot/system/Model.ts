4aecb634a6a0a8b400bdfc3b62c9e2859d69384f
Krivega <mr.krivega@gmail.com>
2026-04-16 13:49:06 +0300

    feat(demo): enhance SystemStatusModel with state flag methods and update type imports

--- demo/statusesRoot/system/Model.ts
+++ demo/statusesRoot/system/Model.ts
@@ -1,28 +1,53 @@
 import { types } from 'mobx-state-tree';
 
-import { ESystemStatus, sessionSelectors } from '@/index';
+import { ESystemStatus as EState, sessionSelectors } from '@/index';
 
 import type { Instance, SnapshotIn } from 'mobx-state-tree';
 import type { TSessionSnapshot } from '@/index';
 
-export type TSystemStatusSnapshot = { state: ESystemStatus };
+export type TSystemStatusSnapshot = { state: EState };
 
 export const SystemStatusModel = types
   .model({
     state: types.enumeration('SystemStatus', [
-      ESystemStatus.DISCONNECTED,
-      ESystemStatus.DISCONNECTING,
-      ESystemStatus.CONNECTING,
-      ESystemStatus.READY_TO_CALL,
-      ESystemStatus.CALL_CONNECTING,
-      ESystemStatus.CALL_DISCONNECTING,
-      ESystemStatus.CALL_ACTIVE,
+      EState.DISCONNECTED,
+      EState.DISCONNECTING,
+      EState.CONNECTING,
+      EState.READY_TO_CALL,
+      EState.CALL_CONNECTING,
+      EState.CALL_DISCONNECTING,
+      EState.CALL_ACTIVE,
     ]),
   })
   .views((self) => {
     return {
       get snapshot(): TSystemStatusSnapshot {
         return { state: self.state } as TSystemStatusSnapshot;
+      },
+    };
+  })
+  .views((self) => {
+    return {
+      isDisconnected(): boolean {
+        return self.state === EState.DISCONNECTED;
+      },
+      isDisconnecting(): boolean {
+        return self.state === EState.DISCONNECTING;
+      },
+      isConnecting(): boolean {
+        return self.state === EState.CONNECTING;
+      },
+      isReadyToCall(): boolean {
+        return self.state === EState.READY_TO_CALL;
+      },
+      isCallConnecting(): boolean {
+        return self.state === EState.CALL_CONNECTING;
+      },
+      isCallDisconnecting(): boolean {
+        return self.state === EState.CALL_DISCONNECTING;
+      },
+      isCallActive(): boolean {
+        return self.state === EState.CALL_ACTIVE;
       },
     };
   });
@@ -40,5 +65,5 @@ export const createSystemStatusSnapshotFromSession = (
 export type TSystemStatusInstance = Instance<typeof SystemStatusModel>;
 
 export const INITIAL_SYSTEM_STATUS_SNAPSHOT = {
-  state: ESystemStatus.DISCONNECTED,
+  state: EState.DISCONNECTED,
 } as SnapshotIn<typeof SystemStatusModel>;
